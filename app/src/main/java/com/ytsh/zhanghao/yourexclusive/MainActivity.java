a64de7d3ac10cddaaf2c27130bf5f155b05aa8b6
hasee <845479430@qq.com>
2017-10-10 10:17:27 +0800

    首次

--- app/src/main/java/com/ytsh/zhanghao/yourexclusive/MainActivity.java
+++ app/src/main/java/com/ytsh/zhanghao/yourexclusive/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements NewCalendar.NewCa
         Month=new SimpleDateFormat("MMM");
         Day=new SimpleDateFormat("dd");
     }
-
+//kjbkjbjhb
     @Override
     public void onItemPress(Date day) {
         String subMonth=Month.format(day).substring(0,Month.format(day).length()-1);
